2261d199c7543ca82d1fb2d2b929cacb0bbb1e48
Bradley Kisia <1649471+brking@users.noreply.github.com>
2025-12-31 14:50:23 -0500

    Update: Wed 12/31/2025 14:50:23.43 - Testing adding a parameter

--- update.bat
+++ update.bat
@@ -5,13 +5,19 @@ echo  Saving changes and deploying
 echo ========================================
 echo.
 
-REM Commit to Git
-git add .
-git commit -m "Update: %date% %time%"
-git push
+REM Commit to Git with optional message
+if "%~1"=="" (
+    git add .
+    git commit -m "Update: %date% %time%"
+    git push
+) else (
+    git add .
+    git commit -m "Update: %date% %time% - %~1"
+    git push
+)
 
 REM Deploy to S3
-aws s3 sync . s3://khumundu.com --delete --exclude ".git/*" --exclude --exclude ".gitignore" --exclude "deploy.bat" --exclude "update.bat" --exclude "READ ME.txt"
+aws s3 sync . s3://khumundu.com --delete --exclude ".git/*" --exclude ".gitignore" --exclude "deploy.bat" --exclude "update.bat" --exclude "update.bat" --exclude "READ ME.txt"
 
 echo.
 echo ========================================
